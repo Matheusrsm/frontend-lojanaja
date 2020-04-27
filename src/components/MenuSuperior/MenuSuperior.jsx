e10875c6510a9600843094c58a0c8a399b5dc84c
arthurazevedo <arthur.stevam.costa@ccc.ufcg.edu.br>
2020-04-27 19:29:02 -0300

    corrigindo bug do input

--- src/components/MenuSuperior/MenuSuperior.jsx
+++ src/components/MenuSuperior/MenuSuperior.jsx
@@ -11,12 +11,8 @@ class MenuSuperior extends Component {
         <Link to="/product">
           <img id="logo" src={logo} alt="logo" />
         </Link>
-        <Link to="/newProduct"> 
-          <div id="botaoAddProduto" class="btn-group-toggle" data-toggle="buttons">
-            <label class="btn btn-secondary active">
-            <input type="checkbox" checked autocomplete="off">Adicionar Produto</input>
-            </label>
-          </div>
+        <Link to="/newProduct">
+          Adicionar Produto
         </Link>
       </nav>
     );
